5a79f639ceb96ede284a248899a57be44aa03882
caiwx86 <caiwenxiu0806@163.com>
2025-04-02 19:25:44 +0800

    默认不下载AdguardHome bin文件，减少固件占用空间

--- patches/preset-adguardhome.sh
+++ patches/preset-adguardhome.sh
@@ -26,10 +26,13 @@ sed -i "s|workdir.*|workdir '/tmp/AdGuardHome'|g" $adguard_conf
 #sed -i "s|option workdir '/etc/AdGuardHome'|option workdir '/opt/AdGuardHome'|" $adguard_conf
 # sed -i "s|option configpath '/etc/AdGuardHome.yaml'|option configpath '/opt/AdGuardHome/AdGuardHome.yaml'|" $adguard_conf
 
-# 更新为AdguardHome最新版本
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_$CLASH_KERNEL | awk -F '"' '{print $4}')
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
-chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+down_bin=False
+if [[ "$down_bin" == "True" ]]; then
+    # 更新为AdguardHome最新版本
+    AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_$CLASH_KERNEL | awk -F '"' '{print $4}')
+    wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+    chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+fi
 
 # 添加AdguardHome DHCP脚本
 install -Dm755 "$GITHUB_WORKSPACE/patches/adguard_update_dhcp_leases.sh" "$OPENWRT_PATH/files/usr/bin/adguard_update_dhcp_leases.sh"
